f3791fca7ca6e13b449d56bc5dede62321ee3e71
My PC <My PC@192.168.1.5>
2023-11-03 14:57:26 +0530

    committing text2 to BrancX

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//class created by amruta
+		System.out.println("HI from test2 class.");
+	}
+
+}
